8c44505ffa83c161e8e7e592c116be54ec2572e8
Dosanko528 <kurutoga528@gmail.com>
2025-02-07 22:57:08 +0900

    ポケモンリスト表示用の関数コンポーネント作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React  from "react";
+import { pokemonList } from "./data/pokemon";
+import PokemonCard  from "./components/PokemonCard";
 
-function App() {
-  const [count, setCount] = useState(0)
 
+//React関数型コンポーネントを作成
+const App: React.FC = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="p-6">
+      <h1 className="text-3xl font-bold text-center mb-4">ポケモンリスト</h1>
+      <div className="grid grid-cols-2 gap-4">
+        {pokemonList.map((pokemon) => (
+          <PokemonCard key={pokemon.id} pokemon={pokemon} />
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
